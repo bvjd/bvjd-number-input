2518124f78b7a6fc1ccbee244bbdc477adaedc6c
Jose David <bvjd91@gmail.com>
2017-10-20 16:37:21 -0600

    0.1.0

---
+++ dist/amd/bvjd-number-input.js
@@ -0,0 +1,8 @@
+/*bvjd-number-input@0.0.0#bvjd-number-input*/
+define(['exports'], function (exports) {
+    'use strict';
+    Object.defineProperty(exports, '__esModule', { value: true });
+    exports.default = function () {
+        return 'This is the bvjd-number-input plugin';
+    };
+});
---
+++ dist/cjs/bvjd-number-input.js
@@ -0,0 +1,6 @@
+/*bvjd-number-input@0.0.0#bvjd-number-input*/
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.default = function () {
+    return 'This is the bvjd-number-input plugin';
+};
